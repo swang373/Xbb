93229cdbc138e02e1df13989fbad32fe66d81e9d
Gael Perrin <gael.ludovic.perrin@cern.ch>
2015-07-26 17:06:35 +0200

    Added the two macro for the ISR studies

---
+++ plugins/VHj_Pt.C
@@ -0,0 +1,45 @@
+#include "TLorentzVector.h"
+
+#include <math.h>
+#include <vector>
+
+
+double VHj_Pt(double V_pt, double V_eta, double V_phi, double V_mass, double H_pt, double H_eta, double H_phi, double H_mass, vector<TLorentzVector> O_Jets, double PhiCut, double VHPtCut){
+
+  //Build the V, H, V+H vector
+  TLorentzVector V, H, VH,VHj;
+  V.SetPtEtaPhiM(V_pt,V_eta,V_phi,V_mass);
+  H.SetPtEtaPhiM(H_pt,H_eta,H_phi,H_mass);
+  VH = V+H;
+
+  //Apply VHPt Cut
+  if(VH.Pt() < VHPtCut){
+
+    return -1;
+
+    //Passes VHPt cut
+  }else{
+
+    double maxpt = 0;
+    int ISRidx = -1;
+
+    //Apply phi cut and keep ISR candiate only
+    for(unsigned int i = 0; i < O_Jets.size(); ++i){
+
+      if( abs(O_Jets[i].Phi() - VH.Phi())  < M_PI - PhiCut ) continue;
+      if( O_Jets[i].Pt() < maxpt ) continue;
+      maxpt = O_Jets[i].Pt();
+      ISRidx = i;
+
+    }
+
+    //No ISR Jets found
+    if(ISRidx == -1) return -1;
+
+    VHj = VH + O_Jets[ISRidx];
+
+    return VHj.Pt();
+
+  }
+
+}
