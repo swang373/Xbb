02f2040d7a1dab91bab8a21940391327033e7699
Silvio <silvio.donato@cern.ch>
2015-10-02 18:05:14 +0200

    update Zvv .sh & .ini

--- python/ZvvHbb13TeVSteps/Step1_trainReg.sh
+++ python/ZvvHbb13TeVSteps/Step1_trainReg.sh
@@ -1,3 +1,1 @@
-setenv TMPDIR /scratch/sdonato/VHbbRun2/CMSSW_7_4_12_patch4/src/Xbb/tmp
-
-bash runAll.sh ciaociao ZvvHbb13TeV trainReg >& log/logTrainRegZH &
+bash runAll.sh ciaociao ZvvHbb13TeV trainReg >& log/logTrainRegression &
