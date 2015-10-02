d5e6fdc5275614cd669b739ffc58b70550b15da1
Silvio <silvio.donato@cern.ch>
2015-10-02 17:45:09 +0200

    save all BDT/regression weights in python/weights folder

--- python/train.py
+++ python/train.py
@@ -42,7 +42,7 @@ gui=opts.verbose
 global_rescale=2.
 
 #get locations:
-MVAdir=config.get('Directories','vhbbpath')+'/data/'
+MVAdir=config.get('Directories','vhbbpath')+'/python/weights/'
 samplesinfo=config.get('Directories','samplesinfo')
 
 #systematics
