64a1ed445c44926e530822c66887e99e5f870c55
Silvio <silvio.donato@cern.ch>
2015-10-29 12:31:11 +0100

    Zvv updates

---
+++ python/ZvvHbb13TeVSteps/Step6_plotBDTbins.sh
@@ -0,0 +1,3 @@
+bash runAll.sh BDTNoMjj_CR ZvvHbb13TeV plot > & log/logPlotBDTNoMjj_CR &
+bash runAll.sh BDTNoMjj_LowPurity ZvvHbb13TeV plot > & log/logPlotBDTNoMjj_LowPurity &
+bash runAll.sh BDTNoMjj_HighPurity ZvvHbb13TeV plot > & log/logPlotBDTNoMjj_HighPurity &
