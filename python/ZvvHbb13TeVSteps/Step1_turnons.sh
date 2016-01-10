3afc37c7578c5d5b14a76f8e7010593c6b53f64a
Silvio <silvio.donato@cern.ch>
2016-01-10 19:18:24 +0100

    Zvv config

---
+++ python/ZvvHbb13TeVSteps/Step1_turnons.sh
@@ -0,0 +1,20 @@
+bash runAll.sh TurnOnEleDen ZvvHbb13TeV plot > & log/logPlotTurnOnEleDen &
+bash runAll.sh TurnOnEleNum ZvvHbb13TeV plot > & log/logPlotTurnOnEleNum &
+bash runAll.sh TurnOnMuDen ZvvHbb13TeV plot > & log/logPlotTurnOnMuDen &
+bash runAll.sh TurnOnMuNum ZvvHbb13TeV plot > & log/logPlotTurnOnMuNum &
+
+bash runAll.sh TurnOnTTEleDen ZvvHbb13TeV plot > & log/logPlotTurnOnTTEleDen &
+bash runAll.sh TurnOnTTEleNum ZvvHbb13TeV plot > & log/logPlotTurnOnTTEleNum &
+bash runAll.sh TurnOnTTMuDen ZvvHbb13TeV plot > & log/logPlotTurnOnTTMuDen &
+bash runAll.sh TurnOnTTMuNum ZvvHbb13TeV plot > & log/logPlotTurnOnTTMuNum &
+
+bash runAll.sh TurnOnQCDEleDen ZvvHbb13TeV plot > & log/logPlotTurnOnQCDEleDen &
+bash runAll.sh TurnOnQCDEleNum ZvvHbb13TeV plot > & log/logPlotTurnOnQCDEleNum &
+bash runAll.sh TurnOnQCDMuDen ZvvHbb13TeV plot > & log/logPlotTurnOnQCDMuDen &
+bash runAll.sh TurnOnQCDMuNum ZvvHbb13TeV plot > & log/logPlotTurnOnQCDMuNum &
+
+bash runAll.sh TurnOnQCDTTEleDen ZvvHbb13TeV plot > & log/logPlotTurnOnQCDTTEleDen &
+bash runAll.sh TurnOnQCDTTEleNum ZvvHbb13TeV plot > & log/logPlotTurnOnQCDTTEleNum &
+bash runAll.sh TurnOnQCDTTMuDen ZvvHbb13TeV plot > & log/logPlotTurnOnQCDTTMuDen &
+bash runAll.sh TurnOnQCDTTMuNum ZvvHbb13TeV plot > & log/logPlotTurnOnQCDTTMuNum &
+
