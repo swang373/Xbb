70cc576cbac93012026e404bbd2a332b49f563cf
Silvio <silvio.donato@cern.ch>
2016-01-28 12:37:49 +0100

    do not run btag_reweighting example when imported

--- python/btag_reweight.py
+++ python/btag_reweight.py
@@ -211,49 +211,50 @@ class Jet :
 #################################################################
 ##Set up offline b-weight calculation
 
-#csvpath = os.environ['CMSSW_BASE']+"/src/VHbbAnalysis/Heppy/data/csv"
-csvpath = "./"
-bweightcalc = BTagWeightCalculator(
-    csvpath + "/csv_rwt_fit_hf_2015_12_14.root",
-    csvpath + "/csv_rwt_fit_lf_2015_12_14.root"
-)
-bweightcalc.btag = "btag"
-
-
-# EXAMPLE (1): per-jet nominal weight
-jet = Jet(50., 1.2, 5, 0.89, bweightcalc.btag)
-
-jet_weight_nominal = bweightcalc.calcJetWeight(
-    jet, kind="final", systematic="nominal",
+if __name__ == "__main__":
+    #csvpath = os.environ['CMSSW_BASE']+"/src/VHbbAnalysis/Heppy/data/csv"
+    csvpath = "./"
+    bweightcalc = BTagWeightCalculator(
+        csvpath + "/csv_rwt_fit_hf_2015_12_14.root",
+        csvpath + "/csv_rwt_fit_lf_2015_12_14.root"
     )
-#print "Nominal jet weight: ", jet_weight_nominal
-
-# EXAMPLE (2): per-jet systematic up/down weight
-for syst in ["JES", "LF", "HF", "LFStats1", "LFStats2", "HFStats1", "HFStats2", "cErr1", "cErr2"]:
-    for sdir in ["Up", "Down"]:
-        jet_weight_shift = bweightcalc.calcJetWeight(
-            jet, kind="final", systematic=syst+sdir
-            )
-        print syst, sdir, ": ", jet_weight_shift
+    bweightcalc.btag = "btag"
 
 
-# EXAMPLE (3): the nominal event weight
-jet1 = Jet(50., -1.2, 5, 0.99, bweightcalc.btag)
-jet2 = Jet(30., 1.8, 4, 0.2, bweightcalc.btag)
-jet3 = Jet(100., 2.2, 0, 0.1, bweightcalc.btag)
-jet4 = Jet(20., 0.5,-5, 0.6, bweightcalc.btag)
-jets = [jet1,jet2,jet3,jet4]
+    # EXAMPLE (1): per-jet nominal weight
+    jet = Jet(50., 1.2, 5, 0.89, bweightcalc.btag)
 
-event_weight_nominal = bweightcalc.calcEventWeight(
-    jets, kind="final", systematic="nominal",
-    )
-print "Nominal event weight: ", event_weight_nominal
+    jet_weight_nominal = bweightcalc.calcJetWeight(
+        jet, kind="final", systematic="nominal",
+        )
+    #print "Nominal jet weight: ", jet_weight_nominal
+
+    # EXAMPLE (2): per-jet systematic up/down weight
+    for syst in ["JES", "LF", "HF", "LFStats1", "LFStats2", "HFStats1", "HFStats2", "cErr1", "cErr2"]:
+        for sdir in ["Up", "Down"]:
+            jet_weight_shift = bweightcalc.calcJetWeight(
+                jet, kind="final", systematic=syst+sdir
+                )
+            print syst, sdir, ": ", jet_weight_shift
 
 
-# EXAMPLE (4): the systematic up/down event weight
-for syst in ["JES", "LF", "HF", "LFStats1", "LFStats2", "HFStats1", "HFStats2", "cErr1", "cErr2"]:
-    for sdir in ["Up", "Down"]:
-        event_weight_shift = bweightcalc.calcEventWeight(
-            jets, kind="final", systematic=syst+sdir
-            )
-        print syst, sdir, ": ", event_weight_shift
+    # EXAMPLE (3): the nominal event weight
+    jet1 = Jet(50., -1.2, 5, 0.99, bweightcalc.btag)
+    jet2 = Jet(30., 1.8, 4, 0.2, bweightcalc.btag)
+    jet3 = Jet(100., 2.2, 0, 0.1, bweightcalc.btag)
+    jet4 = Jet(20., 0.5,-5, 0.6, bweightcalc.btag)
+    jets = [jet1,jet2,jet3,jet4]
+
+    event_weight_nominal = bweightcalc.calcEventWeight(
+        jets, kind="final", systematic="nominal",
+        )
+    print "Nominal event weight: ", event_weight_nominal
+
+
+    # EXAMPLE (4): the systematic up/down event weight
+    for syst in ["JES", "LF", "HF", "LFStats1", "LFStats2", "HFStats1", "HFStats2", "cErr1", "cErr2"]:
+        for sdir in ["Up", "Down"]:
+            event_weight_shift = bweightcalc.calcEventWeight(
+                jets, kind="final", systematic=syst+sdir
+                )
+            print syst, sdir, ": ", event_weight_shift
