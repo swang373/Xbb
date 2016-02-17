9b9ba285db477aebde4cb58f4dce3b4a0e82f0ee
Silvio <silvio.donato@cern.ch>
2016-02-17 19:11:13 +0100

    update doFakeMET

--- python/ZvvHbb13TeVmacros/fakePlot.sh
+++ python/ZvvHbb13TeVmacros/fakePlot.sh
@@ -1,5 +1,5 @@
-bash runAll.sh Nothing ZvvHbb13TeV plot > & log/logPlotNothing &
-bash runAll.sh NoQCD1 ZvvHbb13TeV plot > & log/logPlotNoQCD1 &
+#bash runAll.sh Nothing ZvvHbb13TeV plot > & log/logPlotNothing &
+#bash runAll.sh NoQCD1 ZvvHbb13TeV plot > & log/logPlotNoQCD1 &
 bash runAll.sh NoQCD2 ZvvHbb13TeV plot > & log/logPlotNoQCD2 &
 bash runAll.sh NoQCD3 ZvvHbb13TeV plot > & log/logPlotNoQCD3 &
 #bash runAll.sh AllBDT ZvvHbb13TeV plot > & log/logPlotAllBDT &
