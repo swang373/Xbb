7958282c7eebf4bb75dcc371fe44cb8fe321aa9d
perrozzi <perrozzi@cern.ch>
2016-03-18 12:14:28 +0100

    adjusted parameters

--- python/ZllHbb13TeVmacros/BDT_trainGrid_Launcher.py
+++ python/ZllHbb13TeVmacros/BDT_trainGrid_Launcher.py
@@ -2,7 +2,7 @@ import os,sys,subprocess
 
 # steering parameters
 BDT_list = ['ZllBDT_highVpt']
-max_running_processes = 6
+max_running_processes = 8
 waiting_time_before_retry = 60 # in seconds
 
 ##################################################
@@ -22,7 +22,7 @@ for BDT_point in BDT_list:
     # get the number of running processes
     command = 'ps aux | grep '+user.strip("\n")+' | grep train | grep runAll.sh | wc -l'
     print 'before running',BDT_point,'checking running trainings with the string',command
-    running_trainings = int(subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read())
+    running_trainings = int(int(subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read())/2)
     print 'running_trainings are',running_trainings
     # submit a bdt point
     subprocess.Popen('cd ..; sh runAll.sh '+BDT_point+' '+input_folder+' train 2>&1 > ZllHbb13TeVmacros/logs/'+BDT_point+'.log; cd - &', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
