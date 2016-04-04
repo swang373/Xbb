ae042f79e00e69704ce4121469031a848f248c2d
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-04-04 22:13:00 +0200

    Organize and manually parse configuration files
    This has been personally instructive to see what options exist and
    the logical flow of the configuration. I have added a modified steps
    folder, changing how the commands are called because that many jobs
    cannot be launched in the background on lxplus machines.

---
+++ python/SeanZvvHbb13TeVSteps/Step0_prep.sh
@@ -0,0 +1,47 @@
+# Signal Samples
+bash runAll.sh ZH   ZvvHbb13TeV prep >& log/logPrepZH;
+bash runAll.sh ggZH ZvvHbb13TeV prep >& log/logPrepggZH;
+bash runAll.sh WH   ZvvHbb13TeV prep >& log/logPrepWH;
+
+# W+Jets Samples
+bash runAll.sh WJetsHT0      ZvvHbb13TeV prep >& log/logPrepWJetsHT0;
+bash runAll.sh WJetsMadHT100 ZvvHbb13TeV prep >& log/logPrepWJetsMadHT100;
+bash runAll.sh WJetsMadHT200 ZvvHbb13TeV prep >& log/logPrepWJetsMadHT200;
+bash runAll.sh WJetsMadHT400 ZvvHbb13TeV prep >& log/logPrepWJetsMadHT400;
+bash runAll.sh WJetsMadHT600 ZvvHbb13TeV prep >& log/logPrepWJetsMadHT600;
+
+# Z+Jets Samples
+bash runAll.sh ZJetsHT0      ZvvHbb13TeV prep >& log/logPrepZJetsHT0;
+bash runAll.sh ZJetsMadHT100 ZvvHbb13TeV prep >& log/logPrepZJetsMadHT100;
+bash runAll.sh ZJetsMadHT200 ZvvHbb13TeV prep >& log/logPrepZJetsMadHT200;
+bash runAll.sh ZJetsMadHT400 ZvvHbb13TeV prep >& log/logPrepZJetsMadHT400;
+bash runAll.sh ZJetsMadHT600 ZvvHbb13TeV prep >& log/logPrepZJetsMadHT600;
+
+# TTbar Samples
+bash runAll.sh TT    ZvvHbb13TeV prep >& log/logPrepTT;
+bash runAll.sh TTPow ZvvHbb13TeV prep >& log/logPrepTTPow;
+
+# Single Top Samples
+bash runAll.sh ST_s          ZvvHbb13TeV prep >& log/logPrepST_s;
+bash runAll.sh ST_t          ZvvHbb13TeV prep >& log/logPrepST_t;
+bash runAll.sh ST_tW_antitop ZvvHbb13TeV prep >& log/logPrepST_tW_antitop;
+bash runAll.sh ST_tW_top     ZvvHbb13TeV prep >& log/logPrepST_tW_top;
+
+# QCD Samples
+bash runAll.sh QCDHT100  ZvvHbb13TeV prep >& log/logPrepQCDHT100;
+bash runAll.sh QCDHT200  ZvvHbb13TeV prep >& log/logPrepQCDHT200;
+bash runAll.sh QCDHT300  ZvvHbb13TeV prep >& log/logPrepQCDHT300;
+bash runAll.sh QCDHT500  ZvvHbb13TeV prep >& log/logPrepQCDHT500;
+bash runAll.sh QCDHT700  ZvvHbb13TeV prep >& log/logPrepQCDHT700;
+bash runAll.sh QCDHT1000 ZvvHbb13TeV prep >& log/logPrepQCDHT1000;
+bash runAll.sh QCDHT1500 ZvvHbb13TeV prep >& log/logPrepQCDHT1500;
+bash runAll.sh QCDHT2000 ZvvHbb13TeV prep >& log/logPrepQCDHT2000;
+
+# Diboson Samples
+bash runAll.sh WWpythia  ZvvHbb13TeV prep >& log/logPrepWWpythia;
+bash runAll.sh WZpythia  ZvvHbb13TeV prep >& log/logPrepWZpythia;
+bash runAll.sh ZZpythia  ZvvHbb13TeV prep >& log/logPrepZZpythia;
+bash runAll.sh ZZTo2Q2Nu ZvvHbb13TeV prep >& log/logPrepZZTo2Q2Nu;
+
+bash runAll.sh MET ZvvHbb13TeV prep >& log/logPrepMET;
+
---
+++ python/SeanZvvHbb13TeVSteps/doLinkToData.sh
@@ -0,0 +1,5 @@
+mkdir DataLink
+cd DataLink/
+cp -r -s /gpfs/ddn/srm/cms/store/user/arizzi/VHBBHeppyV15/* .
+rm -r */*PromptReco-v4
+cp -r -s /gpfs/ddn/srm/cms/store/user/arizzi/VHBBHeppyV15a/*
