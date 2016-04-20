7bebed79fa704e0108889aa8d6288b0053954dd6
perrozzi <perrozzi@cern.ch>
2016-04-20 17:56:41 +0200

    add listFiles_for_singlePrep.py to get file list from dataset through DAS query

---
+++ python/ZllHbb13TeVmacros/listFiles_for_singlePrep.py
@@ -0,0 +1,71 @@
+import glob, sys, os, re
+
+vetoed_datasets = [
+                   "BulkGravTohhTohbbhbb_narrow_M",
+                   "DYBJetsToNuNu_Zpt",
+                   "DYJetsToNuNu_TuneCUETP8M1_13TeV",
+                   "DYJetsToQQ_HT180_13TeV",
+                   "GluGluHToBB_M120_13TeV_powheg_pythia8",
+                   "GluGluToBulkGravitonToHHTo4B_M",
+                   "GluGluToHHTo4B_node_",
+                   "MuEnrichedPt5",
+                   "QCD_Pt_",
+                   ]
+
+VHBBHeppy_version = 'VHBBHeppyV21'
+
+# intput_file = str(VHBBHeppy_version)+'.txt'
+intput_file = 'DYB.txt'
+
+das = "../das_client.py"
+
+def cleanList(l):
+    l = [w.strip() for w in l]
+    l = filter(lambda x: len(x)>0, l)
+    return l
+
+def getFiles(dataset):
+    files = os.popen('python {0} --query=\"file dataset={1} instance=prod/phys03\" --limit=0'.format(das, dataset)).read().split("\n")
+    return sorted(cleanList(files))
+
+if __name__ == "__main__":
+    datasets = open(intput_file).readlines()
+    datasets = cleanList(datasets)
+    counter=1
+
+    if not os.path.isdir(VHBBHeppy_version+"_files"): os.system("mkdir "+VHBBHeppy_version+"_files")
+
+    for dataset in datasets:
+
+        # for vetoed_dataset in vetoed_datasets:
+            # vetoed = re.search(vetoed_dataset,dataset)
+            # break
+        # if vetoed:
+            # # print 'dataset',dataset,'vetoed',vetoed
+            # continue
+
+        # print 'dataset',dataset,'vetoed',vetoed
+        # continue
+
+        filename = (dataset[1:]).split("/")[0].replace("/","_")
+
+        ext = ''
+        if '_ext' in dataset:
+            filename = filename+'_ext'+(dataset[1:]).split("_ext")[1][:1]
+
+        filename = filename+'__'+(dataset[1:]).split("/")[1].split(filename[:10])[0].replace("/","_")[:-1]
+
+        if 'Run' in dataset:
+            filename = (dataset[1:]).split("/")[0].replace("/","_")+(dataset[1:])[:-38].split((dataset[1:]).split("/")[0].replace("/","_"))[2]
+
+        if os.path.isfile(VHBBHeppy_version+"_files/"+filename+".txt"):
+            print "TERREMOTO E TRAGEDIA, "+VHBBHeppy_version+"_files/"+filename+".txt from dataset ",dataset,"already exists!!!"
+            # os.system("sleep 30")
+
+        outfile = open(VHBBHeppy_version+"_files/"+filename+".txt", "w")
+        files = getFiles(dataset)
+        print 'retrieving info on dataset',dataset,'txt filename',filename,'counter '+str(counter)+'/'+str(len(datasets))
+        counter=counter+1
+        for file in files:
+            outfile.write(str(file)+'\n')
+        outfile.close()
