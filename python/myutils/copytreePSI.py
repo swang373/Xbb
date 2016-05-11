aa29bf52d3b51a834d11bef97e9f00b338a0e571
Gael Perrin <gael.ludovic.perrin@cern.ch>
2016-05-11 21:33:20 +0200

    fixed bug in copytreePSI.py. Process was stoped if file was missing

--- python/myutils/copytreePSI.py
+++ python/myutils/copytreePSI.py
@@ -12,7 +12,7 @@ def copySingleFile(whereToLaunch,inputFile,outputFile,Acut,remove_branches):
 
         if not input:
           print 'input file NOT EXISTING:',inputFile
-          input.Close()
+          #input.Close()
           return
 
         __tmpPath = os.environ["TMPDIR"]
@@ -81,7 +81,7 @@ def copytreePSI(pathIN,pathOUT,prefix,newprefix,folderName,Aprefix,Acut,config,f
 
     for filename_ in filenames:
         if '.root' in filename_ :
-            if hasattr('Configuration','use_ntuples_from_CERN') and config.get('Configuration','use_ntuples_from_CERN'):
+            if eval(config.get('Configuration','use_ntuples_from_CERN')):
                 filename_ = filename_.replace('/store/user/arizzi','/store/group/phys_higgs/hbb/ntuples/V21/user/arizzi')
             inputFiles.append('root://xrootd-cms.infn.it//'+filename_.rstrip('\n'))
 
