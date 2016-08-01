839d9f51e14188a99b97c84f9f5df9228c97acfa
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-08-01 16:21:35 -0500

    Begin cleaning of configuration files

--- python/ZvvHbb13TeVSteps/Step6_CRplot.sh
+++ python/ZvvHbb13TeVSteps/Step6_CRplot.sh
@@ -1,9 +1,8 @@
-bash runAll.sh CR_Nothing ZvvHbb13TeV plot > & log/logPlotCR_Nothing &
-bash runAll.sh CR_WLight ZvvHbb13TeV plot > & log/logPlotCR_WLight &
-bash runAll.sh CR_Wbb ZvvHbb13TeV plot > & log/logPlotCR_Wbb &
-bash runAll.sh CR_ZLight ZvvHbb13TeV plot > & log/logPlotCR_ZLight &
-bash runAll.sh CR_Zbb ZvvHbb13TeV plot > & log/logPlotCR_Zbb &
-bash runAll.sh CR_QCD ZvvHbb13TeV plot > & log/logPlotCR_QCD &
-bash runAll.sh CR_TTbarTight ZvvHbb13TeV plot > & log/logPlotCR_TTbarTight &
-
-
+bash runAll.sh Signal ZvvHbb13TeV plot >& log/logPlotSignal;
+bash runAll.sh SignalTight ZvvHbb13TeV plot >& log/logPlotSignalTight;
+bash runAll.sh CR_WLight ZvvHbb13TeV plot >& log/logPlotCR_WLight;
+bash runAll.sh CR_Wbb ZvvHbb13TeV plot >& log/logPlotCR_Wbb;
+bash runAll.sh CR_ZLight ZvvHbb13TeV plot >& log/logPlotCR_ZLight;
+bash runAll.sh CR_Zbb ZvvHbb13TeV plot >& log/logPlotCR_Zbb;
+bash runAll.sh CR_QCD ZvvHbb13TeV plot >& log/logPlotCR_QCD;
+bash runAll.sh CR_TTbarTight ZvvHbb13TeV plot >& log/logPlotCR_TTbarTight;
