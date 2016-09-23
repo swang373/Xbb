113274d70e77ed3e098e44a06f0e9222f93d3add
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-09-23 23:16:10 +0200

    First Round of Control Region Plots

--- python/SeanZvvHbb13TeVSteps/Run_CRPlots.sh
+++ python/SeanZvvHbb13TeVSteps/Run_CRPlots.sh
@@ -1,18 +1,18 @@
-bash runAll.sh CR_LowPtTTbar SeanZvvHbb13TeV plot >& log/logPlotCR_LowPtTTbar;
-bash runAll.sh CR_LowPtWLight SeanZvvHbb13TeV plot >& log/logPlotCR_LowPtWLight;
-bash runAll.sh CR_LowPtWbb SeanZvvHbb13TeV plot >& log/logPlotCR_LowPtWbb;
-bash runAll.sh CR_LowPtZLight SeanZvvHbb13TeV plot >& log/logPlotCR_LowPtZLight;
-bash runAll.sh CR_LowPtZbb SeanZvvHbb13TeV plot >& log/logPlotCR_LowPtZbb;
-bash runAll.sh CR_LowPtQCD SeanZvvHbb13TeV plot >& log/logPlotCR_LowPtQCD;
-bash runAll.sh SignalLowPt SeanZvvHbb13TeV plot >& log/logPlotSR_LowPt;
-bash runAll.sh SignalTightLowPt SeanZvvHbb13TeV plot >& log/logPlotSR_LowPtTight;
+bash runAll.sh LowPt_TTbar SeanZvvHbb13TeV plot >& log/Plot_LowPt_TTbar;
+bash runAll.sh LowPt_Wlight SeanZvvHbb13TeV plot >& log/Plot_LowPt_Wlight;
+bash runAll.sh LowPt_Wbb SeanZvvHbb13TeV plot >& log/Plot_LowPt_Wbb;
+bash runAll.sh LowPt_Zlight SeanZvvHbb13TeV plot >& log/Plot_LowPt_Zlight;
+bash runAll.sh LowPt_Zbb SeanZvvHbb13TeV plot >& log/Plot_LowPt_Zbb;
+bash runAll.sh LowPt_QCD SeanZvvHbb13TeV plot >& log/Plot_LowPt_QCD;
+bash runAll.sh LowPt_Signal SeanZvvHbb13TeV plot >& log/Plot_LowPt_Signal;
+bash runAll.sh LowPt_SignalTight SeanZvvHbb13TeV plot >& log/Plot_LowPt_SignalTight;
 
-bash runAll.sh CR_HighPtTTbar SeanZvvHbb13TeV plot >& log/logPlotCR_HighPtTTbar;
-bash runAll.sh CR_HighPtWLight SeanZvvHbb13TeV plot >& log/logPlotCR_HighPtWLight;
-bash runAll.sh CR_HighPtWbb SeanZvvHbb13TeV plot >& log/logPlotCR_HighPtWbb;
-bash runAll.sh CR_HighPtZLight SeanZvvHbb13TeV plot >& log/logPlotCR_HighPtZLight;
-bash runAll.sh CR_HighPtZbb SeanZvvHbb13TeV plot >& log/logPlotCR_HighPtZbb;
-bash runAll.sh CR_HighPtQCD SeanZvvHbb13TeV plot >& log/logPlotCR_HighPtQCD;
-bash runAll.sh SignalHighPt SeanZvvHbb13TeV plot >& log/logPlotSR_HighPt;
-bash runAll.sh SignalTightHighPt SeanZvvHbb13TeV plot >& log/logPlotSR_HighPtTight;
+bash runAll.sh HighPt_TTbar SeanZvvHbb13TeV plot >& log/Plot_HighPt_TTbar;
+bash runAll.sh HighPt_Wlight SeanZvvHbb13TeV plot >& log/Plot_HighPt_Wlight;
+bash runAll.sh HighPt_Wbb SeanZvvHbb13TeV plot >& log/Plot_HighPt_Wbb;
+bash runAll.sh HighPt_Zlight SeanZvvHbb13TeV plot >& log/Plot_HighPt_Zlight;
+bash runAll.sh HighPt_Zbb SeanZvvHbb13TeV plot >& log/Plot_HighPt_Zbb;
+bash runAll.sh HighPt_QCD SeanZvvHbb13TeV plot >& log/Plot_HighPt_QCD;
+bash runAll.sh HighPt_Signal SeanZvvHbb13TeV plot >& log/Plot_HighPt_Signal;
+bash runAll.sh HighPt_SignalTight SeanZvvHbb13TeV plot >& log/Plot_HighPt_SignalTight;
 
