3f735d9ceaefa98edd70619252fbe8da53b7cdca
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-09-27 11:22:09 +0200

    Limit to ICHEP, progress up to SFs

--- python/SeanZvvHbb13TeVSteps/Run_SF.sh
+++ python/SeanZvvHbb13TeVSteps/Run_SF.sh
@@ -1,6 +1,6 @@
-bash runAll.sh SF_QCD SeanZvvHbb13TeV dc >& log/logDatacardSF_QCD;
-bash runAll.sh SF_Zbb SeanZvvHbb13TeV dc >& log/logDatacardSF_Zbb;
-bash runAll.sh SF_ZLight SeanZvvHbb13TeV dc >& log/logDatacardSF_ZLight;
-bash runAll.sh SF_Wbb SeanZvvHbb13TeV dc >& log/logDatacardSF_Wbb;
-bash runAll.sh SF_WLight SeanZvvHbb13TeV dc >& log/logDatacardSF_WLight;
-bash runAll.sh SF_TTbar SeanZvvHbb13TeV dc >& log/logDatacardSF_TTbar;
+#bash runAll.sh SF_QCD SeanZvvHbb13TeV dc >& log/Datacard_SF_QCD;
+bash runAll.sh SF_Zbb SeanZvvHbb13TeV dc >& log/Datacard_SF_Zbb;
+bash runAll.sh SF_Zlight SeanZvvHbb13TeV dc >& log/Datacard_SF_Zlight;
+bash runAll.sh SF_Wbb SeanZvvHbb13TeV dc >& log/Datacard_SF_Wbb;
+bash runAll.sh SF_Wlight SeanZvvHbb13TeV dc >& log/Datacard_SF_Wlight;
+bash runAll.sh SF_TT SeanZvvHbb13TeV dc >& log/Datacard_SF_TT;
