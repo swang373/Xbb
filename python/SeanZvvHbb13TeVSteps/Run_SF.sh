9b77c4c6938b3a04755de0e3bd1d9885db93bb9f
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-09-30 14:12:18 +0200

    Fix missing option in datacards.ini

--- python/SeanZvvHbb13TeVSteps/Run_SF.sh
+++ python/SeanZvvHbb13TeVSteps/Run_SF.sh
@@ -1,6 +1,6 @@
-bash runAll.sh SF_QCD SeanZvvHbb13TeV dc >& log/Datacard_SF_QCD;
-bash runAll.sh SF_Zbb SeanZvvHbb13TeV dc >& log/Datacard_SF_Zbb;
-bash runAll.sh SF_Zlight SeanZvvHbb13TeV dc >& log/Datacard_SF_Zlight;
-bash runAll.sh SF_Wbb SeanZvvHbb13TeV dc >& log/Datacard_SF_Wbb;
-bash runAll.sh SF_Wlight SeanZvvHbb13TeV dc >& log/Datacard_SF_Wlight;
-bash runAll.sh SF_TT SeanZvvHbb13TeV dc >& log/Datacard_SF_TT;
+bash runAll.sh SF_HighPt_QCD SeanZvvHbb13TeV dc >& log/Datacard_SF_HighPt_QCD;
+bash runAll.sh SF_HighPt_Zbb SeanZvvHbb13TeV dc >& log/Datacard_SF_HighPt_Zbb;
+bash runAll.sh SF_HighPt_Zlight SeanZvvHbb13TeV dc >& log/Datacard_SF_HighPt_Zlight;
+bash runAll.sh SF_HighPt_Wbb SeanZvvHbb13TeV dc >& log/Datacard_SF_HighPt_Wbb;
+bash runAll.sh SF_HighPt_Wlight SeanZvvHbb13TeV dc >& log/Datacard_SF_HighPt_Wlight;
+bash runAll.sh SF_HighPt_TT SeanZvvHbb13TeV dc >& log/Datacard_SF_HighPt_TT;
