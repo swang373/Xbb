4f4d35ca3b3f0c199334bdd16a741364bec82cbb
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-10-02 05:02:52 +0200

    BDT more or less ready for ICHEP

---
+++ python/SeanZvvHbb13TeVSteps/Run_Eval_3.sh
@@ -0,0 +1,47 @@
+#bash runAll.sh ZH SeanZvvHbb13TeV eval >& log/Evaluate_ZH;
+#bash runAll.sh ggZH SeanZvvHbb13TeV eval >& log/Evaluate_ggZH;
+#bash runAll.sh WminusH SeanZvvHbb13TeV eval >& log/Evaluate_WminusH;
+#bash runAll.sh WplusH SeanZvvHbb13TeV eval >& log/Evaluate_WplusH;
+#
+#bash runAll.sh WJetsHT0 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT0;
+#bash runAll.sh WJetsHT100 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT100;
+#bash runAll.sh WJetsHT200 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT200;
+#bash runAll.sh WJetsHT400 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT400;
+#bash runAll.sh WJetsHT600 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT600;
+#bash runAll.sh WJetsHT800 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT800;
+#bash runAll.sh WJetsHT1200 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT1200;
+#bash runAll.sh WJetsHT2500 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT2500;
+#
+#bash runAll.sh ZJetsHT100 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT100;
+#bash runAll.sh ZJetsHT200 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT200;
+#bash runAll.sh ZJetsHT400 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT400;
+#bash runAll.sh ZJetsHT600 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT600;
+#bash runAll.sh ZJetsHT800 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT800;
+#bash runAll.sh ZJetsHT1200 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT1200;
+#bash runAll.sh ZJetsHT2500 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT2500;
+#
+#bash runAll.sh TT SeanZvvHbb13TeV eval >& log/Evaluate_TT;
+
+bash runAll.sh ST_s SeanZvvHbb13TeV eval >& log/Evaluate_ST_s;
+bash runAll.sh ST_t_antitop SeanZvvHbb13TeV eval >& log/Evaluate_ST_t_antitop;
+bash runAll.sh ST_t_top SeanZvvHbb13TeV eval >& log/Evaluate_ST_t_top;
+bash runAll.sh ST_tW_antitop SeanZvvHbb13TeV eval >& log/Evaluate_ST_tW_antitop;
+bash runAll.sh ST_tW_top SeanZvvHbb13TeV eval >& log/Evaluate_ST_tW_top;
+
+bash runAll.sh QCDHT100 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT100;
+bash runAll.sh QCDHT200 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT200;
+bash runAll.sh QCDHT300 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT300;
+bash runAll.sh QCDHT500 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT500;
+bash runAll.sh QCDHT700 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT700;
+bash runAll.sh QCDHT1000 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT1000;
+bash runAll.sh QCDHT1500 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT1500;
+bash runAll.sh QCDHT2000 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT2000;
+
+#bash runAll.sh WW SeanZvvHbb13TeV eval >& log/Evaluate_WW;
+#bash runAll.sh WZ SeanZvvHbb13TeV eval >& log/Evaluate_WZ;
+#bash runAll.sh ZZ SeanZvvHbb13TeV eval >& log/Evaluate_ZZ;
+#
+#bash runAll.sh MET_Run2016B SeanZvvHbb13TeV eval >& log/Evaluate_MET2016B;
+#bash runAll.sh MET_Run2016C SeanZvvHbb13TeV eval >& log/Evaluate_MET2016C;
+#bash runAll.sh MET_Run2016D SeanZvvHbb13TeV eval >& log/Evaluate_MET2016D;
+
--- python/SeanZvvHbb13TeVSteps/Run_Evaluation.sh
+++ python/SeanZvvHbb13TeVSteps/Run_Evaluation.sh
@@ -1,45 +1,47 @@
-bash runAll.sh ZH SeanZvvHbb13TeV eval >& log/logEvalZH;
-bash runAll.sh ggZHpow SeanZvvHbb13TeV eval >& log/logEvalggZH;
-bash runAll.sh WH SeanZvvHbb13TeV eval >& log/logEvalWH;
+bash runAll.sh ZH SeanZvvHbb13TeV eval >& log/Evaluate_ZH;
+bash runAll.sh ggZH SeanZvvHbb13TeV eval >& log/Evaluate_ggZH;
+bash runAll.sh WminusH SeanZvvHbb13TeV eval >& log/Evaluate_WminusH;
+bash runAll.sh WplusH SeanZvvHbb13TeV eval >& log/Evaluate_WplusH;
 
-bash runAll.sh WJetsMadHT100 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT100;
-bash runAll.sh WJetsMadHT200 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT200;
-bash runAll.sh WJetsMadHT400 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT400;
-bash runAll.sh WJetsMadHT600 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT600;
-bash runAll.sh WJetsMadHT800 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT800;
-bash runAll.sh WJetsMadHT1200 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT1200;
-bash runAll.sh WJetsMadHT2500 SeanZvvHbb13TeV eval >& log/logEvalWJetsMadHT2500;
+bash runAll.sh WJetsHT0 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT0;
+bash runAll.sh WJetsHT100 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT100;
+bash runAll.sh WJetsHT200 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT200;
+bash runAll.sh WJetsHT400 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT400;
+bash runAll.sh WJetsHT600 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT600;
+bash runAll.sh WJetsHT800 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT800;
+bash runAll.sh WJetsHT1200 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT1200;
+bash runAll.sh WJetsHT2500 SeanZvvHbb13TeV eval >& log/Evaluate_WJetsHT2500;
 
-bash runAll.sh ZJetsMadHT100 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT100;
-bash runAll.sh ZJetsMadHT200 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT200;
-bash runAll.sh ZJetsMadHT400 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT400;
-bash runAll.sh ZJetsMadHT600 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT600;
-bash runAll.sh ZJetsMadHT800 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT800;
-bash runAll.sh ZJetsMadHT1200 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT1200;
-bash runAll.sh ZJetsMadHT2500 SeanZvvHbb13TeV eval >& log/logEvalZJetsMadHT2500;
+bash runAll.sh ZJetsHT100 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT100;
+bash runAll.sh ZJetsHT200 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT200;
+bash runAll.sh ZJetsHT400 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT400;
+bash runAll.sh ZJetsHT600 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT600;
+bash runAll.sh ZJetsHT800 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT800;
+bash runAll.sh ZJetsHT1200 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT1200;
+bash runAll.sh ZJetsHT2500 SeanZvvHbb13TeV eval >& log/Evaluate_ZJetsHT2500;
 
-bash runAll.sh TTPow SeanZvvHbb13TeV eval >& log/logEvalTTPow;
+bash runAll.sh TT SeanZvvHbb13TeV eval >& log/Evaluate_TT;
 
-bash runAll.sh ST_s SeanZvvHbb13TeV eval >& log/logEvalST_s;
-bash runAll.sh ST_t_antitop SeanZvvHbb13TeV eval >& log/logEvalST_t_antitop;
-bash runAll.sh ST_t_top SeanZvvHbb13TeV eval >& log/logEvalST_t_top;
-bash runAll.sh ST_tW_antitop SeanZvvHbb13TeV eval >& log/logEvalST_tW_antitop;
-bash runAll.sh ST_tW_top SeanZvvHbb13TeV eval >& log/logEvalST_tW_top;
+bash runAll.sh ST_s SeanZvvHbb13TeV eval >& log/Evaluate_ST_s;
+bash runAll.sh ST_t_antitop SeanZvvHbb13TeV eval >& log/Evaluate_ST_t_antitop;
+bash runAll.sh ST_t_top SeanZvvHbb13TeV eval >& log/Evaluate_ST_t_top;
+bash runAll.sh ST_tW_antitop SeanZvvHbb13TeV eval >& log/Evaluate_ST_tW_antitop;
+bash runAll.sh ST_tW_top SeanZvvHbb13TeV eval >& log/Evaluate_ST_tW_top;
 
-bash runAll.sh QCDHT100 SeanZvvHbb13TeV eval >& log/logEvalQCDHT100;
-bash runAll.sh QCDHT200 SeanZvvHbb13TeV eval >& log/logEvalQCDHT200;
-bash runAll.sh QCDHT300 SeanZvvHbb13TeV eval >& log/logEvalQCDHT300;
-bash runAll.sh QCDHT500 SeanZvvHbb13TeV eval >& log/logEvalQCDHT500;
-bash runAll.sh QCDHT700 SeanZvvHbb13TeV eval >& log/logEvalQCDHT700;
-bash runAll.sh QCDHT1000 SeanZvvHbb13TeV eval >& log/logEvalQCDHT1000;
-bash runAll.sh QCDHT1500 SeanZvvHbb13TeV eval >& log/logEvalQCDHT1500;
-bash runAll.sh QCDHT2000 SeanZvvHbb13TeV eval >& log/logEvalQCDHT2000;
+bash runAll.sh QCDHT100 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT100;
+bash runAll.sh QCDHT200 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT200;
+bash runAll.sh QCDHT300 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT300;
+bash runAll.sh QCDHT500 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT500;
+bash runAll.sh QCDHT700 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT700;
+bash runAll.sh QCDHT1000 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT1000;
+bash runAll.sh QCDHT1500 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT1500;
+bash runAll.sh QCDHT2000 SeanZvvHbb13TeV eval >& log/Evaluate_QCDHT2000;
 
-bash runAll.sh WWpythia SeanZvvHbb13TeV eval >& log/logEvalWWpythia;
-bash runAll.sh WZpythia SeanZvvHbb13TeV eval >& log/logEvalWZpythia;
-bash runAll.sh ZZpythia SeanZvvHbb13TeV eval >& log/logEvalZZpythia;
+bash runAll.sh WW SeanZvvHbb13TeV eval >& log/Evaluate_WW;
+bash runAll.sh WZ SeanZvvHbb13TeV eval >& log/Evaluate_WZ;
+bash runAll.sh ZZ SeanZvvHbb13TeV eval >& log/Evaluate_ZZ;
 
-bash runAll.sh MET_Run2016B SeanZvvHbb13TeV eval >& log/logEvalMET;
-bash runAll.sh MET_Run2016C SeanZvvHbb13TeV eval >& log/logEvalMET;
-bash runAll.sh MET_Run2016D SeanZvvHbb13TeV eval >& log/logEvalMET;
+bash runAll.sh MET_Run2016B SeanZvvHbb13TeV eval >& log/Evaluate_MET2016B;
+bash runAll.sh MET_Run2016C SeanZvvHbb13TeV eval >& log/Evaluate_MET2016C;
+bash runAll.sh MET_Run2016D SeanZvvHbb13TeV eval >& log/Evaluate_MET2016D;
 
